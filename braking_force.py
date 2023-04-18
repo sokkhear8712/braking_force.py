9f8bc501c7427dc879c7758eddf77b22d585a769
sokkhear8712 <129651152+sokkhear8712@users.noreply.github.com>
2023-04-18 13:51:29 +0300

    Create braking_force.py

---
+++ braking_force.py
@@ -0,0 +1,7 @@
+mass = float(input("Введите массу автомобиля в тоннах: "))
+acceleration = float(input("Введите ускорение в м/с^2: "))
+friction_coefficient = float(input("Введите коэффициент трения: "))
+
+braking_force = mass * acceleration * friction_coefficient
+
+print(f"Сила
